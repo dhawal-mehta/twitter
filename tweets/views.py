be30669c4af8dc66a8e4182cae7f280a4bc941fe
dhawal-mehta <dhawalmehta144@gmail.com>
2021-04-28 17:03:05 +0530

    created base template and extended it in index.html

--- tweets/views.py
+++ tweets/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 
+
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # print(args, kwargs)
     # test = kwargs
     # print(test, test["id"])
-    html = "<h1>Hello World!</ h1>"
-    return HttpResponse(html)
+    # html = "<h1>Hello World!</ h1>"
+    return render(request, 'tweets/index.html', context={}, status=200)
+
 
 def home_detail_view(request, id, *args, **kwargs):
     data = {
         "id": id,
     }
+
     status=200
     try:
         obj = Tweet.objects.get(id=id)
