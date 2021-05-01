3bc99076df1ad09ed2a7088291cf73bc51cf8557
dhawal-mehta <dhawalmehta144@gmail.com>
2021-05-02 01:28:10 +0530

    changes in tweet model to  add user to every tweet default is 1

--- tweets/models.py
+++ tweets/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 import random
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User,default=1, on_delete=models.CASCADE   )
     content = models.TextField(blank=True, null=True)  
 
     class Meta:
         ordering = ["-id"]
-        
+
+    def __str__(self):
+        return self.content
+         
     def serialize(self):
         return {
             "id": self.id,
