9ac979db5b568be66638698401570b3772a99120
dhawal-mehta <dhawalmehta144@gmail.com>
2021-05-07 06:22:59 +0530

    model changes for liking tweet

--- tweets/models.py
+++ tweets/models.py
@@ -3,10 +3,17 @@ import random
 from django.contrib.auth.models import User
 # Create your models here.
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE   )
     content = models.TextField(blank=True, null=True)  
-
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike  )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ["-id"]
 
