3247eb8ec24a5c329eade3a6ffacbf61c8e04d80
dhawal-mehta <dhawalmehta144@gmail.com>
2021-06-06 01:22:51 +0530

    template and view for login, logout and register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-"""twitter URL Configuration
+""" twitter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views as accountViews
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
-    path('', include('tweets.urls') ),
-    path('api/tweets/', include('tweets.api.urls')),
-]
+    
+    # path('', tweetsViews.tweets_list_view),    
+    path('login/', accountViews.login_view),
+    path('logout/', accountViews.logout_view),
+    path('register/', accountViews.register_view)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
+
+def login_view(request, *args, **kwargs):
+    form = AuthenticationForm(request, data=request.POST or None)
+    
+    if form.is_valid():
+        user_ = form.get_user()
+        login(request, user_)
+        return redirect("/")
+
+    context = {'form': form,
+        'btn_label': "Login",
+        'title': "Login"}
+
+    return render(request, "accounts/login.html", context)
+
+
+def logout_view(request, *args, **kwargs):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/account/login/")
+
+    context = {'form': None,
+        'btn_label': "Are you Sure",
+        'title': "Logout"}
+    
+    return render(request, "accounts/logout.html" , context)
+
+
+def register_view(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=True) 
+        # we can send confirmation email here
+        login(request, user)
+        
+        return redirect("/account/login/")
+
+    context = {'form': form,
+        'btn_label': "Sign Up",
+        'title': "Take me in"}
+    return render(request, "accounts/register.html", context)
