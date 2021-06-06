50fd896f110b2690df03cf0130e5ad75bb9996af
dhawal-mehta <dhawalmehta144@gmail.com>
2021-06-06 19:12:09 +0530

    signal to save profile whenever user  is changed

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,3 +8,8 @@ class Profile(models.Model):
     location = models.CharField(max_length=220, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
 
+
+def  user_did_save(sender, instance, created, *args, **kwargs):
+    Profile.objects.get_or_create(user=instance)
+
+post_save.connect(user_did_save, sender=User)
