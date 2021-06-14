22ee1ecaf7b1849646fcf9d1d994391a1dff50bf
dhawal-mehta <dhawalmehta144@gmail.com>
2021-06-14 15:27:45 +0530

    url for user profile  view

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -13,20 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views as profileViews
+
+'''
+Base endpoint /api/profile
+'''
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
-    path('profile/', include('profiles.urls')),
-    path('', include('tweets.urls') ),
-    path('api/tweets/', include('tweets.api.urls')),
-    path('api/profile/', include('profiles.api.urls')),
+
+    path('<str:username>/follow/', profileViews.user_follow_view),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
