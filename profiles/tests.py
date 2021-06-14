6dafe8431256bad15517b9b43646809ff7284e6c
dhawal-mehta <dhawalmehta144@gmail.com>
2021-06-14 15:29:13 +0530

    test for follow and unfollow

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from .models import Profile
 from django.contrib.auth.models import User 
 
+from rest_framework.test import APIClient
 
 
 class ProfileTestCase(TestCase):
@@ -23,3 +24,53 @@ class ProfileTestCase(TestCase):
         first.profile.followers.add(second)
         self.assertEqual( second.following.count() , 1)
         self.assertEqual( first.following.count() , 0)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user, password="#lolta123#")
+        return client
+
+    def test_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(f"/api/profile/{ self.user2.username }/follow/", {
+            "action": "follow"
+        })
+        
+        data = response.json()
+        count = data.get("followers")
+
+        # print(data, response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(count, 1)
+
+    def test_follow_sameuser_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(f"/api/profile/{ self.user.username }/follow/", {
+            "action": "follow"
+        })
+        
+        data = response.json()
+        count = data.get("followers")
+
+        # print(data, response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(count, 1)
+
+    def test_unfollow_api_endpoint(self):
+
+        self.user.profile.followers.add(self.user2)
+        
+        client = self.get_client()        
+        response = client.post(f"/api/profile/{ self.user2.username }/follow/", {
+            "action": "unfollow"
+        })
+        
+        data = response.json()
+        count = data.get("followers")
+
+        # print(data, response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        self.assertEqual(count, 0)  
