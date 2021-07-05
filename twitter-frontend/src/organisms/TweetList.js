4747d3e40d476e2e2922b6aebc1b9f6abaa55089
dhawal-mehta <dhawalmehta144@gmail.com>
2021-07-05 22:50:38 +0530

    add  btn to load next page of tweets

--- twitter-frontend/src/organisms/TweetList.js
+++ twitter-frontend/src/organisms/TweetList.js
@@ -5,40 +5,52 @@ import {tweetsList } from '../XHR/lookup'
  
 export default function TweetList(props){
     const[tweetsInit, setTweetsInit] = useState([])
+    const [nextUrl, setNextUrl] = useState(null)
     const[tweets, setTweets] = useState([])
 
     let {newTweets, setNewTweets, username } = props
 
     const handleDidRetweet = (newTweet) => {
-    
       setNewTweets([newTweet].concat(newTweets))
-    
     }
     
-
     useEffect(() => {
       const finalLength = props.newTweets.length + tweetsInit.length
-      
-      // console.log(finalLength, tweets, tweetsInit)
-
       if ( finalLength > tweets.length){
         setTweets([...props.newTweets].concat(tweetsInit))
       }
-
     }, [props, tweetsInit, tweets])
 
-    function getTweets(){
-      tweetsList("GET", "tweets/", setTweetsInit, username)
+    function setTweetsInitCallback(response){
+      setTweetsInit(response.results)
+      setNextUrl(response.next)
+    }
+
+    function setTweetsCallback(response){
+      const newTweets = [...tweets].concat(response.results)
+      setTweetsInit(newTweets)
+      setTweets(newTweets)
+      setNextUrl(response.next)
     }
   
-    useEffect(getTweets, [username])
+    useEffect(()=>{
+      tweetsList("GET", "tweets/", setTweetsInitCallback, username)
+    }, [username])
 
-    // console.log("nomal call")
-    // console.log(tweets, "rendering tweets from tweet list")
+    function handleLoadMore(e){
+      e.preventDefault()
+      if (nextUrl !== null){
+        tweetsList("GET", nextUrl.substring(nextUrl.indexOf("api/") + 4) ,setTweetsCallback, username)
+      }
+    }
     return (
       <>
         {
           tweets.map( (item, index) => <Tweet tweet={item} handleDidRetweet={handleDidRetweet} key={index}  className="my-5 py-5 border bg-white"/>)
+        }
+        {
+          nextUrl !== null && 
+          <button onClick={handleLoadMore} className="btn btn-outline-primary">Load More</button>
         }
       </>
     );
